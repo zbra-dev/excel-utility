963eabf6f29cbcebd0d1f45ff7d2ba54cfe3fc12
HQ\jose.luz <HQ\jose.luz@4ebea2ca-68f3-b04c-a73d-bc735a12b2fb>
2011-12-06 19:03:51 +0000

    - Incremented CellTests.
    git-svn-id: file:///home/acunha/Development/zbra/svn_repo/zbra/ZBRACommons/ExcelUtility/Trunk@2065 4ebea2ca-68f3-b04c-a73d-bc735a12b2fb

--- ExcelUtility.UnitTests/Tests/CellTests.cs
+++ ExcelUtility.UnitTests/Tests/CellTests.cs
@@ -4,45 +4,95 @@ using System.Linq;
 using System.Text;
 using Xunit;
 using System.Globalization;
+using ExcelUtility.UnitTests.Util;
+using System.Xml.Linq;
 
 namespace ExcelUtility.UnitTests.Tests
 {
-    public class CellTests : IDisposable
+    public class CellTests
     {
-        ExcelFile excelFile;
+        private string path = @"D:/temp/DefaultWorksheet.xlsx";
+        private string sheetName = "Paosdpoasdp";
+        private ReflectionUtil reflection;
 
         public CellTests()
         {
-            excelFile = ExcelFile.Open(@"D:/temp/DefaultWorksheet.xlsx");
+            reflection = new ReflectionUtil();
         }
 
         [Fact]
         public void TestDoubleValue()
         {
             const double doubleValue = 3.78942147;
+            const string stringValue = "Blá!";
+            const string fakeStringValue = "123";
+            const char charNumber = 'B';
 
-            IWorksheet worksheet = excelFile.OpenWorksheet("Paosdpoasdp");
-            var cell = worksheet.GetCell("A4");
-            cell.DoubleValue = doubleValue;
-            /*var decendants = cell.Data.Descendants("v");
-            Assert.NotNull(decendants);
-            var descendant = decendants.FirstOrDefault();
-            Assert.NotNull(descendant);
-            var value =  descendant.Value;
-            Assert.NotNull(value);
-            var convertedValue = Convert.ToDouble(value, CultureInfo.InvariantCulture);
-            Assert.Equal(doubleValue, convertedValue);
-             */ 
+            //put cell names in vars...
+
+            using (var excelFile = ExcelFile.Open(path))
+            {
+                IWorksheet worksheet = excelFile.OpenWorksheet(sheetName);
+
+                Assert.DoesNotThrow(() => worksheet.GetCell("c3"));
+                var cellLowerCase = worksheet.GetCell("c3");
+                Assert.NotNull(cellLowerCase);
+
+                var doubleValueCell = worksheet.GetCell("A4");
+                Assert.NotNull(doubleValueCell);
+                doubleValueCell.DoubleValue = doubleValue;
+
+                var stringValueCell = worksheet.GetCell("A5");
+                Assert.NotNull(stringValueCell);
+                stringValueCell.StringValue = stringValue;
+
+                var fakeStringValueCell = worksheet.GetCell("A6");
+                Assert.NotNull(fakeStringValueCell);
+                fakeStringValueCell.StringValue = fakeStringValue;
+
+                var charNumberValueCell = worksheet.GetCell("A7");
+                Assert.NotNull(charNumberValueCell);
+                charNumberValueCell.DoubleValue = charNumber;
+            }
+
+            using (var excelFile = ExcelFile.Open(path))
+            {
+                IWorksheet worksheet = excelFile.OpenWorksheet(sheetName);
+
+                var sheetData = (XElement)reflection.GetValue(reflection.GetValue(worksheet, "sheetData"), "data");
+                var cellList = sheetData.Descendants(sheetData.GetDefaultNamespace() + "c");
+
+                var doubleValueCell = cellList.Where(c => c.Attribute("r").Value == "A4").FirstOrDefault();
+                Assert.NotNull(doubleValueCell);
+                Assert.Equal(doubleValue, Convert.ToDouble(doubleValueCell.Descendants(sheetData.GetDefaultNamespace() + "v").First().Value, NumberFormatInfo.InvariantInfo));
+
+                //Certificate that it is a string inside.
+                var stringValueCell = cellList.Where(c => c.Attribute("r").Value == "A5").FirstOrDefault();
+                Assert.NotNull(stringValueCell);
+                var tProperty = stringValueCell.Attribute("t");
+                Assert.NotNull(tProperty);
+                Assert.Equal("s", tProperty.Value);
+                //Check if value isn't the inserted string.
+                Assert.NotEqual(stringValue, stringValueCell.Descendants(sheetData.GetDefaultNamespace() + "v").First().Value);
+
+                //Certificate that it is a string inside.
+                var fakeNumberValueCell = cellList.Where(c => c.Attribute("r").Value == "A6").FirstOrDefault();
+                Assert.NotNull(stringValueCell);
+                var fakeTProperty = stringValueCell.Attribute("t");
+                Assert.NotNull(fakeTProperty);
+                Assert.Equal("s", fakeTProperty.Value);
+                //Check if value isn't the inserted fake string.
+                Assert.NotEqual(fakeStringValue, stringValueCell.Descendants(sheetData.GetDefaultNamespace() + "v").First().Value);
+
+                var charNumberValueCell = cellList.Where(c => c.Attribute("r").Value == "A7").FirstOrDefault();
+                Assert.NotNull(charNumberValueCell);
+                Assert.Equal(charNumber, Convert.ToInt32(charNumberValueCell.Descendants(sheetData.GetDefaultNamespace() + "v").First().Value));
+            }
         }
 
         public void TestStringValue()
         {
 
-        }
-
-        public void Dispose()
-        {
-            excelFile.Dispose();
         }
     }
 }
